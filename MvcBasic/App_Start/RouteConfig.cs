0927816a445312df73c0d4d5c18a676c9225d85d
YuChen <xup6m45p1880@gmail.com>
2020-06-22 23:51:40 +0800

    123

--- MvcBasic/App_Start/RouteConfig.cs
+++ MvcBasic/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MvcBasic
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Friends", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
